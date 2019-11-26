d807390b73d96ad1957286a24ba7d22911519f3a
ushapandyan <32410703+ushapandyan@users.noreply.github.com>
2019-11-26 17:46:26 +0530

    change build file

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-testFile="src/test/java/coverage_metrics/CoverageMetricsTest.java"
+#testFile="src/test/java/coverage_metrics/CoverageMetricsTest.java"
+testFile="src/test/java/size_metrics/SizeMetricsTest.java"
 
-if [ "$1" = "-h" ] || [ "$1" = "-?" ]; then
-	echo "Usage: $0 [-h|-?] [<scanner-properties>]"
-	echo ""
-	echo "Example: $0 -Dsonar.host.url=http://localhost:9000"
-	exit 0
-elif [ "$1" = "1" ] || [ "$1" = "2" ] || [ "$1" = "all" ]; then
-	ut=$1
-	shift
-	cp $testFile.ut$ut $testFile
-fi
+#if [ "$1" = "-h" ] || [ "$1" = "-?" ]; then
+#	echo "Usage: $0 [-h|-?] [<scanner-properties>]"
+#	echo ""
+#	echo "Example: $0 -Dsonar.host.url=http://localhost:9000"
+#	exit 0
+#elif [ "$1" = "1" ] || [ "$1" = "2" ] || [ "$1" = "all" ]; then
+#	ut=$1
+#	shift
+#	cp $testFile.ut$ut $testFile
+#fi
 
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent install \
    -Dmaven.test.failure.ignore=true \
